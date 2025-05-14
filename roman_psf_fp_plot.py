b44a54d0611b1cf07df7345933b59aa352897f7d
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-05-14 18:02:53 -0400

    Use argparse for plotting

--- roman_psf_fp_plot.py
+++ roman_psf_fp_plot.py
@@ -1,17 +1,17 @@
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
 import sys
-
-usecols = int(sys.argv[1])
+from matplotlib import rcParams
 
 SIGMA2FWHM = 2*np.sqrt(2*np.log(2.))
 
-from matplotlib import rcParams
 
 def update_rcParams(key, val):
     if key in rcParams:
         rcParams[key] = val
+
 
 update_rcParams('font.size', 20)
 update_rcParams('font.family', 'serif')
@@ -38,9 +38,10 @@ update_rcParams('lines.markersize', 10)
 update_rcParams('lines.markeredgewidth', 1)
 update_rcParams('lines.markeredgecolor', 'k')
 
-def roman_sca_plot(data_array, sca_order, ptype='image', cmap='bwr', residual_plot=True, clabel=None, title=None,
-                   return_fig=False, savefig=False, show_sca_id=False, savepath='roman_scas.png'):
 
+def roman_sca_plot(data_array, sca_order, ptype='image', cmap='bwr', residual_plot=True, clabel=None, title=None, vlims=None,
+                   return_fig=False, savefig=False, show_sca_id=False, savepath='roman_scas.png'):
+    from astropy.visualization import ZScaleInterval
     detector = plt.figure(figsize=(10,6),dpi=300)
     nrows, ncols = 55,91
     grid = detector.add_gridspec(nrows=nrows,ncols=ncols,figure=detector,
@@ -64,19 +65,11 @@ def roman_sca_plot(data_array, sca_order, ptype='image', cmap='bwr', residual_pl
 
     # Argument data_array should be an array of len(N SCAs) containing arrays:
     # fake_data = np.array([np.random.rand(14,14)]*len(axs))
-    if ptype=='iimage':
-        vmin, vmax = ZScaleInterval().get_limits(data_array.ravel())
-    else:
-        if usecols in {0, 1, 10+2, 14+2}:
-            vmin, vmax = data_array.min(), data_array.max()
-        elif usecols in {2,}:
-            vmin, vmax = 0.119, 0.143
-        elif usecols in {3, 4}:
-            vmin = -0.23
-            vmax = -vmin
+    if ptype=='image':
+        if vlims is None:
+            vmin, vmax = ZScaleInterval().get_limits(data_array.ravel())
         else:
-            vmax = np.abs(data_array).max()
-            vmin = -vmax
+            vmin, vmax = vlims
 
     sortidx = sca_order.argsort()
     sca_order = sca_order[sortidx]
@@ -88,7 +81,7 @@ def roman_sca_plot(data_array, sca_order, ptype='image', cmap='bwr', residual_pl
             if residual_plot:
                 ends = np.nanmax(np.array([abs(vmin),abs(vmax)]))
                 im = axs[i].imshow(data_array[sca], cmap=cmap,
-                                       norm=MidpointNormalize(midpoint=0,vmin=-ends,vmax=ends))
+                                   norm=MidpointNormalize(midpoint=0,vmin=-ends,vmax=ends))
             else:
                 im = axs[i].imshow(data_array[sca], cmap=cmap, vmin=vmin,vmax=vmax, interpolation="spline16")
         elif ptype=='scatter':
@@ -116,25 +109,74 @@ def roman_sca_plot(data_array, sca_order, ptype='image', cmap='bwr', residual_pl
 
 
 def flipX(sca):
-    if sca %3 != 0:
+    if sca % 3 != 0:
         return slice(None, None, 1)
     else:
         return slice(None, None, -1)
+
 
 def flipY(sca):
-    if sca %3 != 0:
+    if sca % 3 != 0:
         return slice(None, None, -1)
     else:
         return slice(None, None, 1)
 
-def load_all_data(band="J129"):
+
+def load_all_data(band, usecols):
     # data_array = [np.loadtxt(f"roman_psf_bp/{band}_{sca}.txt", delimiter=",")[:,0].reshape(6,6).T[:,::-1] for sca in range(1, 19)]
     data_array = [np.loadtxt(f"roman_psf_bp/{band}_{sca}_8.0x.txt", delimiter=",", usecols=usecols).reshape(6,6).T[::-1,:][flipX(sca),flipY(sca)] for sca in range(1, 19)]
-    data_array =  np.array(data_array)
+    data_array = np.array(data_array)
     if usecols == 2:
         data_array *= SIGMA2FWHM*0.11
     return data_array
 
-band = "R062"
-data_array = load_all_data(band=band)
-roman_sca_plot(data_array, np.array(list(range(1, 19))), ptype="image", residual_plot=False, savefig=True, show_sca_id=True, title=band, clabel=r"PSF FWHM [arcsec]", cmap=cm.cividis, savepath=f"{band}.pdf")
+
+def determine_vlims(data_array, usecols):
+    if usecols in {0, 1, 10+2, 14+2}:
+        vmin, vmax = data_array.min(), data_array.max()
+    elif usecols in {2,}:
+        vmin, vmax = 0.119, 0.143
+    elif usecols in {3, 4}:
+        vmin = -0.23
+        vmax = -vmin
+    else:
+        vmax = np.abs(data_array).max()
+        vmin = -vmax
+
+    return vmin, vmax
+
+
+def main():
+    parser = argparse.ArgumentParser("Roman PSF plotting on the Focal Plane")
+    parser.add_argument("--band", type=str, required=True, help="Name of the bandpass")
+    parser.add_argument("--usecols", type=int, required=True,
+                        help="The column number to plot. See roman_psf_fp.py. "
+                             "clabel might have to be adjusted accordingly."
+                        )
+    parser.add_argument("--clabel", type=str, required=False,
+                        default=r"PSF FWHM [arcsec]", help="Label on the color bar")
+    args = parser.parse_args()
+
+    band = args.band
+    usecols = int(args.usecols)
+
+    clabel = args.clabel
+    data_array = load_all_data(band=band, usecols=usecols)
+    vlims = determine_vlims(data_array, usecols)
+
+    roman_sca_plot(
+        data_array,
+        np.array(list(range(1, 19))),
+        ptype="image",
+        residual_plot=False,
+        savefig=True,
+        show_sca_id=True,
+        title=band,
+        vlims=vlims,
+        clabel=clabel,
+        cmap=cm.cividis,
+        savepath=f"{band}.pdf")
+
+
+if __name__ == "__main__":
+    main()
